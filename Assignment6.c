96dca545ad6ab0f40ba4c15a813f60841ace1774
Bharati-Raja <126557645+Bharati-Raja@users.noreply.github.com>
2023-03-14 23:20:17 +0530

    Update Assignment6.c
    in the above code the program scans input of number of elements of array then elements after that it sorts the array in ascending order .
    After sorting the array it evaluates the expression with all elements [in line 34] i.e is (a[current element]!=a[previous element]) true? if yes then count of distinct variable increased by 1.

--- Assignment6.c
+++ Assignment6.c
@@ -1,6 +1,37 @@
 //Write a C code to find the number of distinct elements in a sorted array.
-
+//this programm was created by Bharati Raja Swami
+#include<stdio.h>
 int main()
 {
-  return 0;
+	int a[10],no_of_elements,count=0,dummy;
+	printf("Enter [number] of elements of array : ");
+	scanf("%d",&no_of_elements);
+	printf("Enter elements of array :\n");
+	for(int i=0;i<no_of_elements;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(int i=0;i<no_of_elements;i++)
+	{
+		for(int j=i+1;j<no_of_elements;j++)
+    {
+	 		if(a[i]>a[j])
+			{
+				dummy=a[i];            //for swapping of positions
+				a[i]=a[j];
+				a[j]=dummy;
+			}
+		}
+	}
+	printf("Array sorted in ascending order :\n");
+	
+	for(int i=0;i<no_of_elements;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	for(int i=0;i<no_of_elements;i++)
+	{
+		if(a[i]!=a[i-1]){count++;}
+	}
+	printf("\n \nCount of distinct variable is = %d",count);
 }

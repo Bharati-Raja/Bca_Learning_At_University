7c6dbd31cfc284b75f6ec8b7623aa7d0e14e02d7
Bharati-Raja <126557645+Bharati-Raja@users.noreply.github.com>
2023-03-14 20:43:29 +0530

    Update Assignment3.c
    in the above code the program scans the decimal number  collects the remainder  of the entered decimal number after deviding by 2  and the quotient is next dividend an array and then displays it in reverse to match the least (L S B) and most significant bit (M S B) is expected manner.
    
    this program was created by Bharati Raja Swami.

--- Assignment3.c
+++ Assignment3.c
@@ -1,7 +1,25 @@
 //Can you help your digital design faculty in correcting the assignment on number conversions by writing a program to convert a decimal number to its binary equivalent using a while loop.
 //Example, lets say input is 7, output should be 111
 
+#include<stdio.h>
 int main()
 {
-  return 0;
+	int deci,n,binary=0,i=0;int remainder[i];
+	printf("Enter the decimal number : ");
+	scanf("%d",&deci);
+	
+	n=deci;
+	while(n!=0)
+	{
+		remainder[i]=n%2;//collects remainder in array (but in reverse order msb at last and lsb at latest position)
+
+		n=n/2;            //reduces decimal number
+		i++;              //increments i for next emmory index of array
+	}
+	printf("\nThe binary conversion of given input is : ");
+	for( j=i-1;j>=0;j--)    // j=i-1 because in while loop last incremented i is 1 larger than execution of its body 
+	
+	{
+		printf("%d",remainder[j]);
+	}
 }

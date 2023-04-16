ce6ebb15a3a89a79e8a184a7d989b0e765a8171d
Bharati-Raja <126557645+Bharati-Raja@users.noreply.github.com>
2023-04-16 14:20:12 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,44 @@
 //input: Anitha
 //output: Aahint 
 
+#include<stdio.h>
 int main()
 {
-  return 0;
+	char i[100];
+	int j[100];
+	printf("enter string   : ");
+    scanf("%s",&i);
+
+	for(int k=0;k<7;k++)
+{
+	j[k]=i[k];
+}
+
+/*printf("\n");	
+for(int k=0;k<7;k++) for integer equivilent
+	{
+		printf("%d ",j[k]);
+	}printf("\n");*/
+	for(int k=0;k<7;k++)                      //for ascending order sorting
+	{
+		for(int l=0;l<k;l++)
+		{
+		if(j[k]<j[l])
+		  {
+			int t=j[l];
+			j[l]=j[k];
+			j[k]=t;
+	      }
+		}
+	}
+
+
+
+	printf("\n");
+	printf("sorted string  : ");//ascending order
+for(int k=1;k<7;k++)
+{
+	printf("%c",j[k]);
+}
+	return 0;
 }

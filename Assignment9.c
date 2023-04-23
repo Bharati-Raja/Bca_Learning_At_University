d67a63891ff33ac3b935d23f334f975022619995
Bharati-Raja <126557645+Bharati-Raja@users.noreply.github.com>
2023-04-23 13:10:21 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,34 @@
 //.       m = 1
 //.       n = 1
 
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
 int main()
 {
+  char string[100];
+  char ch;
+  int arr[26]={0};
+  
+  printf("please enter string : ");
+  scanf("%s",&string);
+  
+  int len=strlen(string)                   //to know string length
+  ,c=97;                                   //as 'a'=97
+  for(int i=0;i<len;i++)                   //till length of string
+  {
+    ch=tolower(string[i]);                 //for turning upper case letters to small(in ctype)
+    if(ch>='a'&&ch<='z')                   // ifit lies in range to avoid special characters
+    {
+    	arr[ch-'a']++;                     //alters only at the index of int array   {if chracter = c which is 99 in int , then index = 99-97=2}
+	}
+  }
+  printf("\nword frequency count from above string : \n");
+  for(int i=0;i<26;i++)
+  {
+    printf("%c ->  %d\n",c,arr[i]);
+    c++;
+  }
   
   return 0;
 }
